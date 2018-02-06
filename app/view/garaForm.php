754564bde279c4f374d35b2552948bdac1a0093c
ClaudioRoncaglio <claudio.roncaglio@gmail.com>
2018-02-06 13:34:21 +0100

    Impostata lunghezza massima textarea nell'oggetto gara

--- app/view/garaForm.php
+++ app/view/garaForm.php
@@ -87,7 +87,7 @@ $customJsScript = '
                     </div>
                     <!-- / modal atto -->
                     <label for="oggetto">Oggetto</label>
-                    <textarea name="oggetto" class="span6"
+                    <textarea name="oggetto" class="span6" maxlength="250"
                         placeholder="Oggeto della gara"><?php echo $oggetto; ?></textarea>
                     <label for="sceltaContraente">Scelta contraente</label> <select
                         name="sceltaContraente" class="span6">
