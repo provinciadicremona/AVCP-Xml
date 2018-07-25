b9a438d54b3cbeb86ce6ed3737236544f5b4c222
Agostino Zanutto <agostino@sfs.it>
2018-07-25 17:27:02 +0200

    corretto il path del require_once con path relativo

--- app/controller/garaController.php
+++ app/controller/garaController.php
@@ -1,28 +1,29 @@
 <?php
 // Controller che gestisce i metodi che agiscono sui lotti
+isset($_GET['do']) || $_GET['do'] = null;
 switch ($_GET['do']){
 case 'cercaGara':
-    require_once 'app/model/garaCerca.php';
+    require_once __DIR__ . '/../model/garaCerca.php';
     break;
 case 'partecipantiGara':
-    require_once 'app/model/garaPartecipanti.php';
+    require_once __DIR__ . '/../model/garaPartecipanti.php';
     break;
 case 'aggiungiDitta':
-    require_once 'app/model/garaPartecipanti.php';
+    require_once __DIR__ . '/../model/garaPartecipanti.php';
     break;
 case 'eliminaDitta':
-    require_once 'app/model/garaPartecipanti.php';
+    require_once __DIR__ . '/../model/garaPartecipanti.php';
     break;
 case 'eliminaAggiudicatari':
-    require_once 'app/model/garaPartecipanti.php';
+    require_once __DIR__ . '/../model/garaPartecipanti.php';
     break;
 case 'eliminaGara':
-    require_once 'app/model/garaElimina.php';
+    require_once __DIR__ . '/../model/garaElimina.php';
     break;
 case 'elencaGareAnno':
-    require_once 'app/model/garaElenca.php';
+    require_once __DIR__ . '/../model/garaElenca.php';
     break;
 default:
-    require_once 'app/model/gara.php';
+    require_once __DIR__ . '/../model/gara.php';
     break;
 }
