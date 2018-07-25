38119f14dbae875a08e7ae7d3d09536b472322ad
Agostino Zanutto <agostino@sfs.it>
2018-07-25 17:42:15 +0200

    corretto il path del require_once con path relativo

--- app/controller/pageController.php
+++ app/controller/pageController.php
@@ -1,8 +1,8 @@
 <?php
 // Controller che gestisce la visualizzazione delle normali pagine
-$page = 'app/view/' . $_GET['do'] . '.php';
+$page = __DIR__ . '/../view/' . $_GET['do'] . '.php';
 if (!empty($_GET['do']) && file_exists($page)) {
     require_once $page;
 } else {
-    require_once 'app/view/index.php';
+    require_once __DIR__ . '/../view/index.php';
 }
