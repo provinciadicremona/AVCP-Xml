2307e94d4442b76e0287a9c8c18feee68f9d4978
Agostino Zanutto <agostino@sfs.it>
2018-07-25 15:28:04 +0200

    inizializzato $_GET['do'] a null se non presente prima dello switch. sostituito i path dei require con i valori di path relativo.

--- app/controller/dittaController.php
+++ app/controller/dittaController.php
@@ -1,22 +1,23 @@
 <?php
 // Controller dei metodi che agiscono sulle ditte
+isset($_GET['do']) || $_GET['do'] = null;
 switch ($_GET['do']){
 case 'cercaDitta':
-    require_once 'app/model/dittaCerca.php';
+    require_once '../model/dittaCerca.php';
     break;
 case 'elencaDitte':
-    require_once 'app/model/ditteElenca.php';
+    require_once '../model/ditteElenca.php';
     break;
 case 'eliminaDitta':
-    require_once 'app/model/dittaElimina.php';
+    require_once '../model/dittaElimina.php';
     break;
 case 'pulisciDitte':
-    require_once 'app/model/dittePulisci.php';
+    require_once '../model/dittePulisci.php';
     break;
 default:
     if (!empty($_POST['codiceFiscale'])) {
         $_GET['codiceFiscale'] = $_POST['codiceFiscale'];
     }
-    require_once 'app/model/ditta.php';
+    require_once '../model/ditta.php';
     break;
 }
