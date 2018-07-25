b4548a28c80e01fb29ce4ce3f7fb576a11f38ee1
Agostino Zanutto <agostino@sfs.it>
2018-07-25 17:24:39 +0200

    corretto il path del require_once con path relativo iniziallizza $_GET['do'] a null prima dello switch se non inizializzato

--- app/controller/dittaController.php
+++ app/controller/dittaController.php
@@ -3,21 +3,21 @@
 isset($_GET['do']) || $_GET['do'] = null;
 switch ($_GET['do']){
 case 'cercaDitta':
-    require_once '../model/dittaCerca.php';
+    require_once __DIR__ . '/../model/dittaCerca.php';
     break;
 case 'elencaDitte':
-    require_once '../model/ditteElenca.php';
+    require_once __DIR__ . '/../model/ditteElenca.php';
     break;
 case 'eliminaDitta':
-    require_once '../model/dittaElimina.php';
+    require_once __DIR__ . '/../model/dittaElimina.php';
     break;
 case 'pulisciDitte':
-    require_once '../model/dittePulisci.php';
+    require_once __DIR__ . '/../model/dittePulisci.php';
     break;
 default:
     if (!empty($_POST['codiceFiscale'])) {
         $_GET['codiceFiscale'] = $_POST['codiceFiscale'];
     }
-    require_once '../model/ditta.php';
+    require_once __DIR__ . '/../model/ditta.php';
     break;
 }
