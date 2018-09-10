1cdef252f83827c43d7c6cb5746fa558f16f94b6
ClaudioRoncaglio <claudio.roncaglio@gmail.com>
2018-09-10 18:34:30 +0200

    Definita query estrazione gare/ditta

--- app/model/dittaGare.php
+++ app/model/dittaGare.php
@@ -2,21 +2,29 @@
     Dettaglio gare ditta
 </h1>
 <?php
-if (!empty($_GET['codiceFiscale'])) {
-    $cf = $db->real_escape_string($_GET['codiceFiscale']);
+if (!empty($_GET['id'])) {
+    $cf = $db->real_escape_string($_GET['id']);
 }
 $query = "
 SELECT
     `d`.*,
     `ld`.id,
+    `ld`.funzione,
     `l`.oggetto,
+    `l`.anno,
     `l`.cig
 FROM
-    `avcp_ditta` AS d,
+    `avcp_ditta` AS `d`,
     `avcp_ld` AS `ld`,
-    `avcp_lotto` AS l
+    `avcp_lotto` AS `l`
 WHERE
-    ld.codiceFiscale = d.codiceFiscale AND d.codiceFiscale LIKE 'RNCCLD75H23I849F' AND ld.funzione LIKE '02-AGGIUDICATARIO' AND l.id = ld.id
-GROUP BY
-    `l`.`id`";
+    `ld`.codiceFiscale = `d`.codiceFiscale AND `d`.codiceFiscale LIKE '".$cf."' AND `l`.id = `ld`.id
+ORDER BY
+    `l`.anno DESC ,
+    `l`.id ASC,
+    `ld`.funzione ASC
+";
+$res = $db->query($query);
+$quanti = $res->num_rows;
+echo $quanti;
 
