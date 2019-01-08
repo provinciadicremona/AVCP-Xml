f91a739a464294f450970fb9e60d6610e7c09356
ClaudioRoncaglio <claudio.roncaglio@gmail.com>
2019-01-08 10:35:11 +0100

    Tolti filtri da model/garaElimina

--- app/model/garaElimina.php
+++ app/model/garaElimina.php
@@ -1,15 +1,16 @@
 <?php
-if (!empty(filter_input(INPUT_GET, 'id', FILTER_VALIDATE_INT))) {
-    $queryVer = "SELECT count(id) as quante, anno FROM avcp_lotto
-        WHERE id = '" . $db->real_escape_string($_GET['id']) . "' GROUP BY anno";
-
+if (array_key_exists('id', $_GET)) {
+    $idDel       = (int) $_GET['id'];
+    $queryVer    = "SELECT count(id) as quante, anno 
+                    FROM avcp_lotto
+                    WHERE id = '" . $db->real_escape_string($idDel) . "'
+                    GROUP BY anno";
     $resVer       = $db->query($queryVer);
     $row          = $resVer->fetch_assoc();
     $numVer       = $row['quante'];
     $anno         = $row['anno'];
     $messElimina  = null;
     $errorElimina = null;
-
     if ($numVer != 1) {
         $messElimina  = "Cancellazione non riuscita!";
         $errorElimina = "La gara che desideri eliminare non esiste!";
@@ -17,8 +18,8 @@ if (!empty(filter_input(INPUT_GET, 'id', FILTER_VALIDATE_INT))) {
         $messElimina  = "Cancellazione non permessa!";
         $errorElimina = "La gara che desideri eliminare si riferisce ad un anno bloccato.";
     } else {
-        $queryDelLotto = "DELETE FROM avcp_lotto WHERE id = '" . $db->real_escape_string($_GET['id']) . "'";
-        $queryDelLd    = "DELETE FROM avcp_ld WHERE id = '" . $db->real_escape_string($_GET['id']) . "'";
+        $queryDelLotto = "DELETE FROM avcp_lotto WHERE id = '" . $db->real_escape_string($idDel) . "'";
+        $queryDelLd    = "DELETE FROM avcp_ld WHERE id = '" . $db->real_escape_string($idDel) . "'";
         $resDelLotto = $db->query($queryDelLotto);
         if ($resDelLotto !== true) {
             $messElimina  = "Cancellazione non riuscita!";
