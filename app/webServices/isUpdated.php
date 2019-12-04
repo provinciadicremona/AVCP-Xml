e9dbd0e9406b7b330fb7d772cea8a71479c6c712
Claudio Roncaglio <claudio.roncaglio@gmail.com>
2019-12-04 18:30:33 +0100

    Inizio a spostare i controlli di versione dal sito a github

--- app/webServices/isUpdated.php
+++ app/webServices/isUpdated.php
@@ -8,72 +8,67 @@
  * © 2013 Provincia di Cremona <sito@provincia.cremona.it>
  * 
  * SPDX-License-Identifier: GPL-3.0-only
-*/
-?>
+ */
 
-<?php
 $outVersion = null;
 // determino la versione attuale:
 $fh = fopen('version.txt', 'r');
-$localVersion = fread($fh, 1024);
+$local= fread($fh, 1024);
 fclose($fh);
+
+/* 
+ * Converto il numero di versione per essere compatibile con 
+ * quello usato su github:
+ *  - In locale: 0_0_0
+ *  - Su github: v0.0.0
+ */
+$localVersion = "v".(strtr($local, '_', '.'));
 
 // crea una risorsa cURL
 $ch = curl_init();
-
-// imposto l'URL da interrogare
-curl_setopt($ch, CURLOPT_URL, 'http://www.provincia.cremona.it/avcp-repo/getVersion.php');
-
+// Imposto come URL da interrogare quello dell'API di github per l'ultima release
+curl_setopt($ch, CURLOPT_URL, 'https://api.github.com/repos/provinciadicremona/AVCP-Xml/releases/latest');
 // escludo l'header dall'output
 curl_setopt($ch, CURLOPT_HEADER, 0);
-
+// Imposto lo useragent col nome del repository altrimenti github nega la risposta
+curl_setopt($ch, CURLOPT_USERAGENT, 'AVCP-Xml');
 // Imposto un massimo di 5 secondi prima di interrompere l'operazione
 curl_setopt($ch, CURLOPT_TIMEOUT, 5);
-
 // Sopprimo l'output sul browser
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-
-// recupero l'URL e lo assegno in variabile
-$currentVersion = curl_exec($ch);
-
+// Eseguo la chiamata e memorizzo la risposta (che è in JSON)
+$rispostaGithub = curl_exec($ch);
+// Converto il JSON in un array
+$githubData = json_decode($rispostaGithub, true); // true per avere un array e non un oggetto
+$currentVersion =  $githubData['tag_name'];
+$releaseTitle = $githubData['name'];
+$downloadZip = '<a href="'.$githubData['zipball_url'].'">Scarica in formato zip</a>';
+$downloadTarball = '<a href="'.$githubData['tarball_url'].'">Scarica in formato tar.gz</a>';
 // chiudo la risorsa cURL
 curl_close($ch);
 // Se hai una vecchia versione
 if ($currentVersion > $localVersion) {
     // Comunico che esiste una nuova versione
-    $currentVersion = str_replace('_', '.', $currentVersion);
     $outVersion .= '
     <div class="alert alert-info">
         <button type="button" class="close" data-dismiss="alert">&times;</button>
         <strong>Una nuova versione di AVCP Xml presente: ' . $currentVersion . '</strong><br />
-        <a href="http://www.provincia.cremona.it/urp/?view=Pagina&amp;id=5354"><i class="icon-download-alt"></i>&nbsp;Scaricala dal sito della Provincia di Cremona</a>
-        </div>' . PHP_EOL;
-
-    // Recupero le note di rilascio
-
-    // crea una risorsa cURL
-    $ch = curl_init();
-
-    // imposto l'URL da interrogare
-    curl_setopt($ch, CURLOPT_URL, 'http://www.provincia.cremona.it/avcp-repo/note');
-
-    // escludo l'header dall'output
-    curl_setopt($ch, CURLOPT_HEADER, 0);
-
-    // Imposto un massimo di 5 secondi prima di interrompere l'operazione
-    curl_setopt($ch, CURLOPT_TIMEOUT, 5);
-
-    // Sopprimo l'output sul browser
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-
-    // recupero l'URL e lo assegno in variabile
-    $noteRilascio = curl_exec($ch);
-    // Comunico le note di rilascio
+    </div>' . PHP_EOL;
+    $outVersion .= "<h2>".$releaseTitle."</h2>".PHP_EOL;
+    // Per convertire il markdown delle note di release in html
+    // uso parsedown https://github.com/erusev/parsedown
+    require_once 'Parsedown.php';
+    $Parsedown = new Parsedown();
+    $noteRilascio = $Parsedown->text($githubData['body']);
     $outVersion .= $noteRilascio;
+    $outVersion .= '
+    <ul>
+        <li>'.$downloadZip.'</li>
+        <li>'.$downloadTarball.'</li>
+    </ul>';
 } else {
     // Comunico che va tutto bene così
     $outVersion .= '<p><strong>Questa versione di AVCP Xml è aggiornata</strong><br />' . PHP_EOL;
     $outVersion .= '<a href="./">Torna all\'homepage</a></p>' . PHP_EOL;
 }
-
-?>
+echo $outVersion;
